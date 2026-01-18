1a889351e5458be7fa0b490e42279f21098d4acc
berrksancakk <kasimberksancak61@gmail.com>
2026-01-18 20:10:24 +0300

    Update plotting.py

--- src/plotting.py
+++ src/plotting.py
@@ -1,21 +1,61 @@
 # -*- coding: utf-8 -*-
-"""
-Grafik çizim fonksiyonları
-"""
+#"""
+#Grafik çizim fonksiyonları
+#"""
+
+#def plot_time(t, x_raw, x_filt, save_path):
+#    """
+#    Ham ve filtrelenmiş sinyali zaman domeninde çizer
+
+#    Parametreler:
+#        t        : zaman vektörü
+#        x_raw   : ham sinyal
+#        x_filt  : filtrelenmiş sinyal
+#        save_path : kayıt edilecek dosya yolu
+#    """
+    # TODO:
+    # matplotlib kullanarak:
+    # - iki sinyali aynı grafikte çiz
+    # - eksen isimleri ve başlık ekle
+    # - grafiği dosyaya kaydet
+#    pass
+    # -- coding: utf-8 --
+#"""
+#Grafik çizim fonksiyonları
+#"""
+
+import matplotlib.pyplot as plt
+
 
 def plot_time(t, x_raw, x_filt, save_path):
     """
     Ham ve filtrelenmiş sinyali zaman domeninde çizer
 
     Parametreler:
-        t        : zaman vektörü
-        x_raw   : ham sinyal
-        x_filt  : filtrelenmiş sinyal
+        t         : zaman vektörü
+        x_raw     : ham sinyal
+        x_filt    : filtrelenmiş sinyal
         save_path : kayıt edilecek dosya yolu
     """
-    # TODO:
-    # matplotlib kullanarak:
-    # - iki sinyali aynı grafikte çiz
-    # - eksen isimleri ve başlık ekle
-    # - grafiği dosyaya kaydet
-    pass
+
+    # Ham sinyali çiz
+    plt.plot(t, x_raw, label="Ham Sinyal")
+
+    # Filtrelenmiş sinyali çiz
+    plt.plot(t, x_filt, label="Filtrelenmiş Sinyal")
+
+    # Eksen isimleri
+    plt.xlabel("Zaman (s)")
+    plt.ylabel("Genlik")
+
+    # Başlık
+    plt.title("Ham ve Filtrelenmiş Sinyal (Zaman Domeni)")
+
+    # Açıklama kutusu
+    plt.legend()
+
+    # Grafiği dosyaya kaydet
+    plt.savefig(save_path)
+
+    # Belleği temizle
+    plt.close()
