fcf69ac3c93b7106e07bd01404cf0fbcbe900de8
berrksancakk <kasimberksancak61@gmail.com>
2026-01-18 20:15:41 +0300

    Update signal_tools.py

--- src/signal_tools.py
+++ src/signal_tools.py
@@ -1,7 +1,32 @@
 # -*- coding: utf-8 -*-
-"""
-Basit sinyal işleme fonksiyonları
-"""
+#"""
+#Basit sinyal işleme fonksiyonları
+#"""
+
+#def moving_average(x, window_size):
+#    """
+#    Hareketli ortalama filtresi
+
+#    Parametreler:
+#        x           : sinyal vektörü
+#        window_size : pencere uzunluğu (int)
+
+#    Dönen:
+#        y : filtrelenmiş sinyal
+#    """
+    # TODO:
+    # 1. her örnek için pencereyi belirle
+    # 2. pencere içindeki ortalamayı hesapla
+    # 3. sonucu yeni bir listeye yaz
+#    pass
+    # -- coding: utf-8 --
+#"""
+#Basit sinyal işleme fonksiyonları
+#"""
+
+x = [10, 12, 11, 15, 14]
+y = moving_average(x, 3)
+print(y)
 
 def moving_average(x, window_size):
     """
@@ -14,8 +39,18 @@ def moving_average(x, window_size):
     Dönen:
         y : filtrelenmiş sinyal
     """
-    # TODO:
-    # 1. her örnek için pencereyi belirle
-    # 2. pencere içindeki ortalamayı hesapla
-    # 3. sonucu yeni bir listeye yaz
-    pass
+
+    y = []  # filtrelenmiş sinyal
+
+    # Her örnek için pencereyi gez
+    for i in range(len(x) - window_size + 1):
+        # 1. pencereyi belirle
+        window = x[i:i + window_size]
+
+        # 2. pencere içindeki ortalamayı hesapla
+        avg = sum(window) / window_size
+
+        # 3. sonucu listeye ekle
+        y.append(avg)
+
+    return y
